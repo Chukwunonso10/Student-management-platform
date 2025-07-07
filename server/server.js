dbf76005b38b3b3de035354066f103ce21a7a342
Chukwunonso10 <kuzuechinonsojude@gmail.com>
2025-07-07 16:07:57 +0100

    configured the origin

--- server/server.js
+++ server/server.js
@@ -23,6 +23,9 @@ app.use(cors({
 app.use('/api/auth', authRoute)
 app.use('/api/students', studentRoute)
 app.use('/api/course', CourseRoute)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
+});
 
 
 app.listen(process.env.PORT, () =>{
