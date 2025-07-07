7663c6f8a0c053745a093566b42efffd9bdddd9b
Chukwunonso10 <kuzuechinonsojude@gmail.com>
2025-07-07 15:57:48 +0100

    configured the origin

--- server/server.js
+++ server/server.js
@@ -16,7 +16,9 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
+app.use(cors({
+    origin: '*'
+}))
 
 app.use('/api/auth', authRoute)
 app.use('/api/students', studentRoute)
