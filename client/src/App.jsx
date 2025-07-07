be0bf51c2e5e5539f8b84db2830fd8374cf0c17c
Chukwunonso10 <kuzuechinonsojude@gmail.com>
2025-07-07 21:07:01 +0100

    setting up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,30 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Dashboard from './pages/Dashboard'
-import Login from './pages/Login'
-import SignUp from './pages/SignUp'
-import ProtectedRoute from './utilities/ProtectedRoute'
+import SignUp from "./pages/SignUp"
+import Login from "@/pages/Login"
+import { Routes, Route } from "react-router-dom"
+
+import { BrowserRouter } from 'react-router-dom'
+import Dashboard from "./pages/Dashboard"
+import ProtectedRoute from "./utilities/ProtectedRoutes"
+import ThemeToggle from "./components/themeToggle"
+
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <div>
+      <ThemeToggle />
+      <BrowserRouter>
       <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/dashboard" element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        } />
         <Route path="/" element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        } />
+                          <ProtectedRoute>  
+                            <Dashboard /> 
+                          </ProtectedRoute>
+          } />
+       
       </Routes>
-    </BrowserRouter>
+      </BrowserRouter>
+    </div>
   )
 }
