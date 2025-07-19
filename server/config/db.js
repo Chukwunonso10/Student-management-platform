74a1791a834d949a5348cd8074b35af0fa239f35
Chukwunonso10 <kuzuechinonsojude@gmail.com>
2025-07-19 12:25:41 +0100

    handling post errors

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const ConnectDB = () => {
-    mongoose.connect(process.env.MONGO_URI)
+    mongoose.connect(process.env.MONGO_URI_PRODUCTION)
     .then(() =>{
         console.log("Connected successfully")
     })
--- server/server.js
+++ server/server.js
@@ -19,10 +19,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-    origin: 'https://student-management-platform-1-frontend.onrender.com',
-    credentials: true
-}))
+app.use(cors())
 
 
 app.use('/api/auth', authRoute)
