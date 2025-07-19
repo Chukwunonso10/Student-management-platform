1ae214d5b9973dfb3de8b819298a4b8ab2b684ba
Chukwunonso10 <kuzuechinonsojude@gmail.com>
2025-07-19 10:17:38 +0100

    bugs

--- server/server.js
+++ server/server.js
@@ -19,7 +19,9 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
+app.use(cors({
+    origin: '*'
+}))
 
 
 app.use('/api/auth', authRoute)
